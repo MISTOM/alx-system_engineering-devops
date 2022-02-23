9ac7d7345947689893d346dc3a1d21bb0c98792b
MISTOM <kigardetom2001@gmail.com>
2022-02-23 22:47:58 +0300

    the sky is the limit not

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# 0. Sky is the limit, let's bring that limit higher
+exec { 'fix_problem':
+  command  => 'sed -i s/15/4096/g /etc/default/nginx; sudo service nginx restart',
+  provider => 'shell'
+}
